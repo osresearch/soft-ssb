f2b247a614caf561f09b79226bd90b8e2b23f3f5
Trammell Hudson <hudson@trmm.net>
2000-01-01 00:02:00 +0000

    pre-generate bits; it was taking too long to compute each time

--- pru-ssb/sdr.c
+++ pru-ssb/sdr.c
@@ -7,8 +7,11 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include "pru.h"
+
+static int sin_table[128];
 
 static uint16_t
 shuffle(
@@ -120,43 +123,112 @@ if (1)
 
 #endif
 
+
+/** Output a PSK31 encoded bit.
+ * Phase controls which sin/cos is used.
+ * ramp_up will ramp up over a few ms
+ * ramp_down will ramp down over a few ms
+ */
 static void
-ssb_output(
-	volatile uint16_t * const buf,
-	float s_sin,
-	float s_cos
+generate_bit(
+	uint16_t * const buf,
+	int phase,
+	int ramp_up,
+	int ramp_down
 )
 {
-	// wait for a signal that we're halfway
-	// so that buf is free.  this should go before, not after
-	while((buf[0] & (1<<8)) != 0)
-		;
-
-	for (int i = 0 ; i < 4096 ; i++)
+	// 32 ms per bit == 313 passes at 102.4 usec per pass
+	for (unsigned j = 0 ; j < 313 ; j++)
 	{
-		// 25 ns per output, 16 outputs per cycle
-		// 400 ns per cycle == 2.5 MHz
-		float t = 256*i*M_PI/2048;
-		float c_sin = sin(t);
-		float c_cos = cos(t);
+		int mag = 1024;
 
-		// 102.4 us per pass
-		// 512 Hz = 1953 us
-		float ssb = c_sin * s_cos - c_cos * s_sin;
+		if (ramp_up && j < 128)
+			mag = sin_table[j/4];
+		if (ramp_down && j > 313 - 128)
+			mag = sin_table[(313 - j - 1)/4];
 
-		buf[i] = shuffle(ssb*128 + 127);
+		for (unsigned i = 0 ; i < 4096 ; i++)
+		{
+			// 25 ns per output, 65536 outputs per cycle
+			// 1638400 ns per cycle == 610 Hz
+			int sig_ti = ((j << 12) + i) / 65536;
+			if (phase)
+				sig_ti += 64;
+
+			int sig_sin = +sin_table[(sig_ti +  0) % 128];
+			int sig_cos = -sin_table[(sig_ti + 32) % 128];
+
+			// 25 ns per output, 16 outputs per cycle
+			// 400 ns per cycle == 2.5 MHz
+			int c_ti = i*8;
+			int c_sin = +sin_table[(c_ti +  0) % 128];
+			int c_cos = -sin_table[(c_ti + 32) % 128];
+
+			// 102.4 us per pass
+			// 512 Hz = 1953 us
+			int ssb = (c_sin * sig_cos - c_cos * sig_sin) / 1024;
+
+			// if we're ramping, scale it
+			ssb = (ssb * mag) / 1024;
+
+			// ssb has range -1024 to 1024;
+			// convert to 0 to 256
+			buf[i] = shuffle(ssb / 8 + 127);
+
+			// mark an unused bit; the pru will zero
+			// this when it is halfway through the buffer.
+		}
 	}
+}
 
-	// mark an unused bit; the pru will zero this when it is halfway
-	// through the buffer.
-	buf[0] |= (1 << 8) | (1 << 15);
 
+static void
+output_bit(
+	volatile uint16_t * const pru_buf,
+	const uint16_t * const bit_buf
+)
+{
+	for (unsigned i = 0 ; i < 313 ; i++)
+	{
+		// wait for a signal that we're halfway
+		// so that buf is free.  this should go before, not after
+		while((pru_buf[0] & (1<<8)) != 0)
+			;
+
+		for (unsigned x = 0 ; x < 4096 ; x++)
+			pru_buf[x] = bit_buf[i*4096 + x];
+		pru_buf[0] |= (1 << 8) | (1 << 15);
+	}
 }
 
 
 int
 main(void)
 {
+	for (int i = 0 ; i < 128 ; i++)
+	{
+		sin_table[i] = sin(i * M_PI * 2 / 128) * 1024;
+	}
+
+	uint16_t * bits[8];
+
+	for (int i = 0 ; i < 8 ; i++)
+	{
+		bits[i] = calloc(2, 4096 * 313);
+		if (bits[i] == NULL)
+			fprintf(stderr, "failed\n");
+	}
+
+
+	generate_bit(bits[0], 0, 0, 0);
+	generate_bit(bits[1], 0, 0, 1);
+	generate_bit(bits[2], 0, 1, 0);
+	generate_bit(bits[3], 0, 1, 1);
+	generate_bit(bits[4], 1, 0, 0);
+	generate_bit(bits[5], 1, 0, 1);
+	generate_bit(bits[6], 1, 1, 0);
+	generate_bit(bits[7], 1, 1, 1);
+
 	pru_t * const pru = pru_init(0);
 
 	uint32_t * const pru_cmd = pru->data_ram;
@@ -167,23 +239,12 @@ main(void)
 	printf("cmd %p\n", pru_cmd);
 	printf("ddr %p (%08x)\n", vram, pru->ddr_addr);
 
-	volatile uint16_t * const buf = pru_cmd;
-	memset(buf, 0, 8192);
-
-	int pass = 10;
-
-	uint32_t t = 0;
+	volatile uint16_t * const buf = (void*) pru_cmd;
 
 	while (1)
 	{
-		float mag = (pass % 256) / 256.0;
-		float s_sin = +sin(pass*M_PI/10) * mag;
-		float s_cos = -cos(pass*M_PI/10) * mag;
-
-		// send 102 usec worth of data
-		ssb_output(buf, s_sin, s_cos);
-
-		pass++;
+		output_bit(buf, bits[3]);
+		output_bit(buf, bits[7]);
 	}
 
 	return 0;
