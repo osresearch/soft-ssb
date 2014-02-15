f7a08aca92395451338544eb708d4c2eca40c27e
Trammell Hudson <hudson@osresearch.net>
2014-02-15 18:17:11 -0500

    ramp up and ramp down appear to work

--- dac.ino
+++ dac.ino
@@ -63,7 +63,10 @@ setup(void)
 	//carrier_timer.begin(ssb_output, 1.5);
 
 	// Controls the period of the carrier output
-	periodic_timer_configure(50);
+	// 50 works out to 60 KHz transmission
+	// 16 calls per cycle == 800 ticks @ 48 Mhz == 60 KHz
+	// (PIT runs at F_BUS=48, not system clock of 72)
+	periodic_timer_configure(100);
 }
 
 
@@ -105,7 +108,7 @@ ssb_output(
 	signal += 2048; // shift to zero offset
 
 	dac_output(signal);
-	i += 8;
+	i += 16;
 }
 
 
@@ -114,26 +117,81 @@ loop(void)
 {
 	uint32_t sig = 0;
 	static uint8_t bytes[] = { 0xA5, 0x00, 0xFF, 0x18, 0xFF };
+	int bit_offset = 0;
+	const int bit_count = sizeof(bytes) * 8;
+	int bit_changed = 1;
+	int cur_bit = 0;
+	int new_bit = 0;
 
 	while (1)
 	{
 		// signal sin and cos
-		const uint16_t ps_i = sig >> 10;
+		// each loop is 960 KHz, so /2048 == 468 Hz
+		const uint16_t ps_i = sig >> 11;
 		const uint16_t pc_i = ps_i + 32 + 64;
 
 		int32_t ps = +sin_table[ps_i % 128];
 		int32_t pc = -sin_table[pc_i % 128];
 
-		// after 1024 cycles, pulse off
-		if (sig & 0x400000)
+#if 1
+		// we want 31.250 Hz for our bit clock,
+		// 960000 / 31.250 == 30720 cycles
+#define ramp_size 8192
+#define bit_len 30720
+
+		if (sig == bit_len)
 		{
-			ps /= 4;
-			pc /= 4;
+			// time for the next bit
+			sig = 0;
+			cur_bit = new_bit;
+		} else
+		if (sig < ramp_size)
+		{
+			// ramp up if this bit changed
+			if (bit_changed)
+			{
+				int mag = sig;
+				ps = (ps * mag) / ramp_size;
+				pc = (pc * mag) / ramp_size;
+			}
+		} else
+		if (sig == (bit_len - ramp_size))
+		{
+			new_bit = bytes[bit_offset / 8];
+			new_bit &= 1 << (bit_offset % 8);
+			if (new_bit != cur_bit)
+				bit_changed = 1;
+			else
+				bit_changed = 0;
+			cur_bit = new_bit;
+			bit_offset++;
+			if (bit_offset == bit_count)
+				bit_offset = 0;
+		} else
+		if (sig > (bit_len - ramp_size))
+		{
+			// ramp down if this bit will change
+			if (bit_changed)
+			{
+				int mag = bit_len - sig;
+				ps = (ps * mag) / ramp_size;
+				pc = (pc * mag) / ramp_size;
+			}
 		}
+#else
+		int mag = (sig >> 18) % 256;
+		ps = (ps * mag) / 256;
+		pc = (pc * mag) / 256;
+#endif
 
 		while (!periodic_timer_expired())
 			;
-		ssb_output(ps, pc);
-		sig += 16;
+
+		if (cur_bit)
+			ssb_output(ps, pc);
+		else
+			ssb_output(pc, ps);
+
+		sig++;
 	}
 }
