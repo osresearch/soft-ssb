6f70875998f311b714c53f615271b8e0a35b9ee4
Trammell Hudson <hudson@osresearch.net>
2014-02-15 13:38:38 -0500

    cleaned up code to show carrier/signal

--- dac.ino
+++ dac.ino
@@ -1,31 +1,43 @@
-float phase = 0.0;
-float twopi = 3.14159 * 2;
-elapsedMicros usec = 0;
+/** \file
+ * Software SSB transmitter for the Teensy 3.1 using the DAC.
+ *
+ * Generates a 1 MHz carrier and then modulates data over SSB.
+ */
 
 uint16_t sin_table[128];
 
-void setup() {
-  analogWriteResolution(12);
-  analogWrite(A14, 0);
+void
+setup(void)
+{
+	analogWriteResolution(12);
+	analogWrite(A14, 0);
 
-  for (int i = 0 ; i < 128 ; i++)
-	sin_table[i] = (1+sin(i * M_PI / 64)) * 512;
+	for (int i = 0 ; i < 128 ; i++)
+		sin_table[i] = (1+sin(i * M_PI / 64)) * 512;
 }
 
 
+static inline void
+dac_output(
+	uint16_t x
+)
+{
+	*(volatile uint16_t*)&(DAC0_DAT0L) = x;
+}
 
-void loop() {
-  //float val = sin(phase) * 2000.0 + 2050.0;
-  unsigned i = 0;
-  uint32_t sig = 0;
-  while(1)
-  {
-     *(volatile uint16_t*)&(DAC0_DAT0L) = (sin_table[i] * ((sig / 65536) % 512)) / 512;
-     i = (i + 16) % 128;
-     sig++;
-  }
- // phase = phase + 0.02;
- // if (phase >= twopi) phase = 0;
-  //while (usec < 500) ; // wait
-  //usec = usec - 500;
+
+void
+loop(void)
+{
+	unsigned i = 0;
+	uint32_t sig = 0;
+
+	while(1)
+	{
+		uint16_t carrier = sin_table[i % 128];
+		uint16_t signal = sin_table[(sig / 65536) % 128];
+		dac_output(carrier * signal / 512);
+		i += 16;
+		sig += 1;
+	}
 }
