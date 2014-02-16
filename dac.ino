e3bdc2d4f884ba55f7688f9cbe0361b30b2d3806
Trammell Hudson <hudson@osresearch.net>
2014-02-15 19:11:56 -0500

    generate 32 ms pulses, attempt to ramp up and down

--- dac.ino
+++ dac.ino
@@ -2,6 +2,8 @@
  * Software SSB transmitter for the Teensy 3.1 using the DAC.
  *
  * Generates the sin/cos of the carrier and then modulates data over SSB.
+ *
+ * We want to generate 16384 passes per bit, 
  */
 
 int16_t sin_table[128];
@@ -63,10 +65,10 @@ setup(void)
 	//carrier_timer.begin(ssb_output, 1.5);
 
 	// Controls the period of the carrier output
-	// 50 works out to 60 KHz transmission
-	// 16 calls per cycle == 800 ticks @ 48 Mhz == 60 KHz
-	// (PIT runs at F_BUS=48, not system clock of 72)
-	periodic_timer_configure(100);
+	// PIT runs at F_BUS=48, not system clock of 72 or 96
+	// 48 MHz / (94 * 8 cycles) == 63.83 KHz transmission
+	// 16384 passes through the loop is 32 ms == 31.25 Hz
+	periodic_timer_configure(94);
 }
 
 
@@ -112,22 +114,59 @@ ssb_output(
 }
 
 
+/*
+// CQ NY3U
+1010110100 C
+11101110100 Q
+1010110100 C
+11101110100 Q
+100
+1101110100	116	78	4E	N
+10111101100	131	89	59	Y
+1111111100	063	51	33	3
+10101011100	125	85	55	U
+
+10101101
+00111011
+10100101
+01101001
+11011101
+00100110
+11101001
+01111011
+00111111
+11001010
+10111000
+*/
+
+static const uint8_t bytes[] = {
+0xAD,
+0x3B,
+0xA5,
+0x69,
+0xDD,
+0x26,
+0xE9,
+0x7B,
+0x3F,
+0xCA,
+0xB8,
+};
+
 void
 loop(void)
 {
 	uint32_t sig = 0;
-	static uint8_t bytes[] = { 0xA5, 0x00, 0xFF, 0x18, 0xFF };
 	int bit_offset = 0;
 	const int bit_count = sizeof(bytes) * 8;
-	int bit_changed = 1;
+	int do_ramp = 1;
 	int cur_bit = 0;
 	int new_bit = 0;
 
 	while (1)
 	{
 		// signal sin and cos
-		// each loop is 960 KHz, so /2048 == 468 Hz
-		const uint16_t ps_i = sig >> 11;
+		const uint16_t ps_i = sig >> 3;
 		const uint16_t pc_i = ps_i + 32 + 64;
 
 		int32_t ps = +sin_table[ps_i % 128];
@@ -136,48 +175,52 @@ loop(void)
 #if 1
 		// we want 31.250 Hz for our bit clock,
 		// 960000 / 31.250 == 30720 cycles
-#define ramp_size 8192
-#define bit_len 30720
+#define ramp_size 2048
+#define bit_len 16384
 
 		if (sig == bit_len)
 		{
 			// time for the next bit
 			sig = 0;
 			cur_bit = new_bit;
-		} else
+		}
+
+if (1)
+{
 		if (sig < ramp_size)
 		{
-			// ramp up if this bit changed
-			if (bit_changed)
+			// ramp up if this bit is unchanged
+			if (!do_ramp)
 			{
-				int mag = sig;
-				ps = (ps * mag) / ramp_size;
-				pc = (pc * mag) / ramp_size;
-			}
-		} else
-		if (sig == (bit_len - ramp_size))
-		{
-			new_bit = bytes[bit_offset / 8];
-			new_bit &= 1 << (bit_offset % 8);
-			if (new_bit != cur_bit)
-				bit_changed = 1;
-			else
-				bit_changed = 0;
-			cur_bit = new_bit;
-			bit_offset++;
-			if (bit_offset == bit_count)
-				bit_offset = 0;
-		} else
-		if (sig > (bit_len - ramp_size))
-		{
-			// ramp down if this bit will change
-			if (bit_changed)
-			{
-				int mag = bit_len - sig;
+				int32_t mag = sig;
 				ps = (ps * mag) / ramp_size;
 				pc = (pc * mag) / ramp_size;
 			}
 		}
+		if (sig == (bit_len - ramp_size))
+		{
+			new_bit = bytes[bit_offset / 8];
+			new_bit >>= 7 - (bit_offset % 8);
+			new_bit &= 1;
+			if (new_bit == 0)
+				do_ramp = 1;
+			else
+				do_ramp = 0;
+			bit_offset++;
+			if (bit_offset == bit_count)
+				bit_offset = 0;
+		}
+		if (sig > (bit_len - ramp_size))
+		{
+			// ramp down if this bit will not change
+			if (!do_ramp)
+			{
+				int32_t mag = bit_len - sig;
+				ps = (ps * mag) / ramp_size;
+				pc = (pc * mag) / ramp_size;
+			}
+		}
+}
 #else
 		int mag = (sig >> 18) % 256;
 		ps = (ps * mag) / 256;
@@ -187,10 +230,14 @@ loop(void)
 		while (!periodic_timer_expired())
 			;
 
-		if (cur_bit)
+#if 0
+		dac_output(ps/2 + 2048);
+#else
+		if (do_ramp && cur_bit)
 			ssb_output(ps, pc);
 		else
 			ssb_output(pc, ps);
+#endif
 
 		sig++;
 	}
